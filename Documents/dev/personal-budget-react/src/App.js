b07cf9014665417f5d0554c0e2c3f61fcb4b038e
Tim Hillmann <55610816+Tubatim13@users.noreply.github.com>
2025-02-23 23:39:47 -0500

    Brought PBudget in

---
+++ Documents/dev/personal-budget-react/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Menu/>
+      <Hero/>
+      <div className='mainContainer'>
+        <Routes>
+          <Route path='/about' element={<AboutPage />}/>
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/' element={<HomePage />}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
